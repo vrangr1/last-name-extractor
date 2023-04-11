185bef6bbc74ab6da86988cfcf3f8a9811b22bf8
vrangr1 <anavprasadanav@gmail.com>
2023-04-11 00:11:04 -0400

    bug fix done; code working properly

--- last_name_extractor.py
+++ last_name_extractor.py
@@ -1,5 +1,11 @@
 import argparse
 
+def contains_uppercase(word: str) -> bool:
+    if '-' in word:
+        return False
+    if len(word) >= 4: return False
+    return any(char.isupper() for char in word)
+
 def last_name_extractor(input_file: str, output_file: str) -> None:
     """Extracts the last name from a file and writes it to a new file."""
     file = open(input_file, 'r')
@@ -7,6 +13,7 @@ def last_name_extractor(input_file: str, output_file: str) -> None:
     file_write = open(output_file, 'w')
     for line in file_lines:
         line = line.strip()
+        og = line
         if line == 'N/A':
             file_write.write("N/A\n")
             continue
@@ -16,8 +23,11 @@ def last_name_extractor(input_file: str, output_file: str) -> None:
         line = line.split()
         assert len(line) > 1, "Line must have at least two words. Current line: " + str(line)
         last_word = line[-1]
-        if last_word[1:].isupper():
-            last_word = line[-2]
+        
+        while contains_uppercase(last_word[1:]):
+            line = line[:-1]
+            last_word = line[-1]
+            assert len(line) > 0, "Name must have something other than designations! Name: " + str(og) + "; line: " + str(line)
         file_write.write(last_word + "\n")
     file.close()
     file_write.close()
